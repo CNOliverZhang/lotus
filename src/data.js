df738bae874495ffc1eb2aa53fa0725160034c2b
CNOliverZhang <cnoliverzhang@gmail.com>
2019-04-28 20:28:20 +0800

    1.9

---
+++ src/data.js
@@ -0,0 +1,20 @@
+var areas = [
+  {
+    index: 0,
+    name: '主楼',
+    style: 'width: 10%; height: 10%; top: 80%; left: 50%;',
+    image: '/images/2.png',
+    description: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容。'
+  },
+  {
+    index: 1,
+    name: '二主楼',
+    style: 'width: 10%; height: 15%; top: 30%; left: 30%;',
+    image: '/images/2.png',
+    description: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容。'
+  }
+]
+
+module.exports = {
+  areas: areas
+}
